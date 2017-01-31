6d39d047f315aa8b40b17b9e79a7abb53e4de474
Victor Delepine <vdelepine@deezer.com>
2017-01-31 09:59:28 +0100

    First global timer thing

--- app/controllers/socket.js
+++ app/controllers/socket.js
@@ -2,83 +2,54 @@ global.io  = io.listen(app.server); // Initialize socket.io
 
 global.timers = {};
 
-function interval(duration, fn){
-  this.baseline = undefined
-
-  this.run = function(){
-    if(this.baseline === undefined){
-      this.baseline = new Date().getTime()
-    }
-    fn()
-    var end = new Date().getTime()
-    this.baseline += duration
-
-    var nextTick = duration - (end - this.baseline)
-    if(nextTick<0){
-      nextTick = 0
-    }
-    (function(i){
-        i.timer = setTimeout(function(){
-        i.run(end)
-      }, nextTick)
-    }(this))
-  }
-}
-
+// timer.run();
 // Listen for client connection to join them in the right room
 io.on('connection', socket => {
-
-	let startTrackEmission = () => {
-		const message = {
-			room: {
-				id: 123,
-			},
-			state: 2,
-			track: 136889434,
-			countDown: 20
-		};
-		console.log(message);
-		socket.broadcast.emit('StartTrackMessage', message);
-	}
-
-	//const timer = new interval(20000, startTrackEmission);
-	//timer.run();
-
-	socket.on('blindtest', function(resp){
-		console.log(resp);
-	})
+  console.log('Somebody joined us');
 
 	socket.on('join', room => {
-		global.timers[room] = global.timers[room] || setInterval(function() {
-			console.log('Track done for room', room);
-			socket.to(room).emit('track-ended');
-		}, 30000);
-
+  	const message = {
+  		room: {
+  			id: 123,
+  		},
+  		state: 2,
+  		track: 136889434,
+  		countDown: 20
+  	};
 		socket.join(room);
+
+		global.timers[room] = global.timers[room] || setInterval(function() {
+			socket.to(room).emit('EndOfTrackMessage', 'END '+new Date());
+      setTimeout(() => {
+  			socket.to(room).emit('StartTrackMessage', message);
+      }, 5000)
+		}, 10000);
+
 	});
 
-	socket.on('guess', function(string) {
-		const room = socket.rooms[socket.id]; // Marche pas
-		if (string === 'Deftones') {
-			socket.to(room).emit('global-guess', {
-				result: true,
-				id: socket.id,
-				message: "Machin a trouvé"
-			});
-			socket.emit('guess', {
-				result: true,
-				message: "Bien joué"
-			});
-		} else {
-			socket.to(room).emit('global-guess', {
-				result: false,
-				id: socket.id,
-				message: "Machin s'est trompé"
-			});
-			socket.emit('guess', {
-				result: false,
-				message: "Boouuhh"
-			});
-		}
+	socket.on('ClientGuessMessage', function(string) {
+    console.log(string);
+		// const room = socket.rooms[socket.id]; // Marche pas
+		// if (string === 'Deftones') {
+		// 	socket.to(room).emit('global-guess', {
+		// 		result: true,
+		// 		id: socket.id,
+		// 		message: "Machin a trouvé"
+		// 	});
+		// 	socket.emit('guess', {
+		// 		result: true,
+		// 		message: "Bien joué"
+		// 	});
+		// } else {
+		// 	socket.to(room).emit('global-guess', {
+		// 		result: false,
+		// 		id: socket.id,
+		// 		message: "Machin s'est trompé"
+		// 	});
+		// 	socket.emit('guess', {
+		// 		result: false,
+		// 		message: "Boouuhh"
+		// 	});
+		// }
 	});
 });
